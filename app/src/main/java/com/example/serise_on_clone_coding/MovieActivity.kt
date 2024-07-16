f89ed9051bd48c1bcdfab0b2a6674091c86aadad
idkos <leegang31@gmail.com>
2024-07-16 09:57:03 +0900

    refactor: 리사이클러뷰 적용

--- app/src/main/java/com/example/serise_on_clone_coding/MovieActivity.kt
+++ app/src/main/java/com/example/serise_on_clone_coding/MovieActivity.kt
@@ -8,8 +8,10 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.serise_on_clone_coding.Data.MovieAdapter
 import com.example.serise_on_clone_coding.databinding.MoviePageBinding
-
+import com.example.serise_on_clone_coding.Data.Movies
 import java.util.Collections
 
 class MovieActivity : AppCompatActivity() {
@@ -18,7 +20,7 @@ class MovieActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = MoviePageBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.movie_page)
+        setContentView(binding.root)
 
         val mainScrollView = binding.customScrollView
         val stickyLayout = binding.llUpperTapBar
@@ -48,13 +50,26 @@ class MovieActivity : AppCompatActivity() {
                 Log.d("LOGGER_TAG", "NOT Movie List")
                 stickyLayout.setBackgroundResource(R.drawable.final_bg_tp)
             }
-
         }
+        // 리사이클러뷰 사용 코드
+        val adapter = MovieAdapter(Movies.movieList.values.toMutableList())
+        binding.rcvHot.adapter = adapter
+        binding.rcvHot.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
 
+        binding.rcvTrend.adapter = adapter
+        binding.rcvTrend.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
 
+        binding.rcvTop.adapter = adapter
+        binding.rcvTop.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
+        binding.rcvUpdated.adapter = adapter
+        binding.rcvUpdated.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
+        // 레이아웃 인플레이터 사용 코드
+        /*
         // HOT & NEW 영화 리스트 생성
-        val hotMovie = findViewById<LinearLayout>(R.id.ll_hotMovie)
-        makeMovieList(hotMovie, false)
+        //val hotMovie = findViewById<LinearLayout>(R.id.ll_hotMovie)
+        //makeMovieList(hotMovie, false)
 
         // 실시간 트렌드  영화 리스트 생성
         val trendMovie = findViewById<LinearLayout>(R.id.ll_trendMovie)
@@ -65,16 +80,18 @@ class MovieActivity : AppCompatActivity() {
 
         val updatedMovie = findViewById<LinearLayout>(R.id.ll_updated_movie)
         makeMovieList(updatedMovie, false)
+        */
+
     }
 
-    fun makeMovieList(movieLayout: LinearLayout, isTopMovie: Boolean) {
+    private fun makeMovieList(movieLayout: LinearLayout, isTopMovie: Boolean) {
         var style = R.layout.poster_list
 
         // 영화 객체 리스트화
         val movieList = Movies.movieList.values.toList()
         // 리스트 순서 섞기
         Collections.shuffle(movieList)
-        //TopMovie면 전체 가져오기, 아닐 시 8개만 가져오기 \
+        //TopMovie면 전체 가져오기, 아닐 시 8개만 가져오기
         val randomMovies = if (isTopMovie) movieList else movieList.take(8)
         if (isTopMovie) {
             style = R.layout.top_list
